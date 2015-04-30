937db1c9b20680f9aeb9f096828b8d917558137e
Hans Stamm <hstamm@hsr.ch>
2015-04-30 16:23:48 +0200

    Prototyp hinzugefügt

---
+++ src/test/java/ch/metzenthin/svm/prototyp/Person.java
@@ -0,0 +1,74 @@
+package ch.metzenthin.svm.prototyp;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="person")
+public class Person {
+
+	public Person() {
+		super();
+	}
+
+	public Person(String lastname, String firstname, Date birth) {
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.birth = birth;
+	}
+
+	@Id
+	@GeneratedValue
+    private int personid;
+
+	@Column(name="lastname")
+	private String lastname;
+
+	@Column(name="firstname")
+	private String firstname;
+
+	@Column(name="birth")
+	private Date birth;
+
+	public int getPersonid() {
+		return personid;
+	}
+
+	public void setPersonid(int personid) {
+		this.personid = personid;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	@Override
+	public String toString() {
+		return "personid=" + personid + ",lastname=" + lastname + ",firstname=" + firstname + ",birth=" + birth;
+	}
+
+}
---
+++ src/test/java/ch/metzenthin/svm/prototyp/PersonsTableModel.java
@@ -0,0 +1,69 @@
+package ch.metzenthin.svm.prototyp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+
+public class PersonsTableModel extends AbstractTableModel {
+
+	private static final long serialVersionUID = 1L;
+	private DataAccess dataAccess;
+
+	PersonsTableModel(DataAccess dataAccess) {
+		super();
+		this.dataAccess = dataAccess;
+	}
+
+	List<Person> persons = new ArrayList<Person>();
+	String[] columns = {"personid", "lastname", "firstname", "birth"};
+	
+	@Override
+	public int getRowCount() {
+		return persons.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return columns.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Person person = persons.get(rowIndex);
+		if (person != null) {
+			Object columnValue = null;
+			switch (columnIndex) {
+			case 0:
+				columnValue = person.getPersonid();
+				break;
+			case 1:
+				columnValue = person.getLastname();
+				break;
+			case 2:
+				columnValue = person.getFirstname();
+				break;
+			case 3:
+				columnValue = person.getBirth();
+				break;
+
+			default:
+				break;
+			}
+			return columnValue;
+		}
+		return null;
+	}
+	
+	public void search() throws Exception {
+		this.persons = dataAccess.query();
+		fireTableDataChanged();
+	}
+
+	public void create(String[] personAttributes) throws Exception {
+		Person person = new Person(personAttributes[0], personAttributes[1], new Date());
+		dataAccess.persist(person);
+		search();
+	}
+}
