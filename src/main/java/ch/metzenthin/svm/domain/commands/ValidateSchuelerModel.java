379ec88b3135f7c4274effbd8361744c7da424ee
Hans Stamm <hstamm@hsr.ch>
2015-06-04 10:43:39 +0200

    Interface für ValidateSchuelerCommand

---
+++ src/main/java/ch/metzenthin/svm/domain/commands/ValidateSchuelerModel.java
@@ -0,0 +1,22 @@
+package ch.metzenthin.svm.domain.commands;
+
+import ch.metzenthin.svm.persistence.entities.Adresse;
+import ch.metzenthin.svm.persistence.entities.Angehoeriger;
+import ch.metzenthin.svm.persistence.entities.Schueler;
+
+/**
+ * @author Hans Stamm
+ */
+public interface ValidateSchuelerModel {
+    Schueler getSchueler();
+    Adresse getAdresseSchueler();
+    Angehoeriger getMutter();
+    Adresse getAdresseMutter();
+    boolean isRechnungsemfpaengerMutter();
+    Angehoeriger getVater();
+    Adresse getAdresseVater();
+    boolean isRechnungsemfpaengerVater();
+    Angehoeriger rechnungsempfaengerDrittperson();
+    Adresse getAdresseRechnungsempfaengerDrittperson();
+    boolean isRechnungsemfpaengerDrittperson();
+}
