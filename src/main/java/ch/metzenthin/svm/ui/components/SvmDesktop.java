5de853c1baa9de51bb465431aeb6f78b57f22f7f
Hans Stamm <hstamm@hsr.ch>
2015-06-11 18:29:55 +0200

    Panel statt InternalFrame

--- src/main/java/ch/metzenthin/svm/ui/components/SvmDesktop.java
+++ src/main/java/ch/metzenthin/svm/ui/components/SvmDesktop.java
@@ -3,8 +3,6 @@ package ch.metzenthin.svm.ui.components;
 import ch.metzenthin.svm.common.SvmContext;
 
 import javax.swing.*;
-import javax.swing.event.InternalFrameEvent;
-import javax.swing.event.InternalFrameListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,13 +12,12 @@ import java.awt.event.KeyEvent;
 /*
  * SVM Applikation
  */
-public class SvmDesktop extends JFrame implements ActionListener, InternalFrameListener {
+public class SvmDesktop extends JFrame implements ActionListener {
 
     private static final long serialVersionUID = 1L;
 
-    private final JDesktopPane desktop;
     private final SvmContext svmContext;
-    private SvmInternalFrame schuelerErfassenFrame;
+    private SchuelerErfassenPanel schuelerErfassenPanel;
 
     public SvmDesktop(SvmContext svmContext) {
         super("SVM Schüler Verwaltung Metzenthin");
@@ -33,13 +30,7 @@ public class SvmDesktop extends JFrame implements ActionListener, InternalFrameL
         setBounds(inset, inset, screenSize.width - inset * 2, screenSize.height - inset * 2);
 
         // Set up the GUI.
-        desktop = new JDesktopPane(); // a specialized layered pane
-        // createFrame(); // create first "window"
-        setContentPane(desktop);
         setJMenuBar(createMenuBar());
-
-        // Make dragging a little faster but perhaps uglier.
-        desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -81,74 +72,29 @@ public class SvmDesktop extends JFrame implements ActionListener, InternalFrameL
     // React to menu selections.
     public void actionPerformed(ActionEvent e) {
         if ("schuelerErfassen".equals(e.getActionCommand())) {
-            SchuelerErfassenPanel schuelerErfassenPanel = new SchuelerErfassenPanel(svmContext);
+            schuelerErfassenPanel = new SchuelerErfassenPanel(svmContext);
             schuelerErfassenPanel.addCloseListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     onSchuelerErfassenFrameAbbrechen();
                 }
             });
-            schuelerErfassenFrame = createFrame(schuelerErfassenPanel.$$$getRootComponent$$$(), "Schüler erfassen", new Dimension(1000, 900));
+            getContentPane().add(schuelerErfassenPanel.$$$getRootComponent$$$());
+            schuelerErfassenPanel.$$$getRootComponent$$$().setVisible(true);
+            revalidate();
         } else { // beenden
             quit();
         }
     }
 
     private void onSchuelerErfassenFrameAbbrechen() {
-        schuelerErfassenFrame.dispose();
-    }
-
-    // Create a new internal frame.
-    private SvmInternalFrame createFrame(JComponent panel, String title, Dimension dimension) {
-        SvmInternalFrame frame = new SvmInternalFrame(panel, title, dimension);
-        frame.setVisible(true); // necessary as of 1.3
-        frame.addInternalFrameListener(this);
-        desktop.add(frame);
-        try {
-            frame.setSelected(true);
-        } catch (java.beans.PropertyVetoException ignore) {
-        }
-        return frame;
+        schuelerErfassenPanel.$$$getRootComponent$$$().setVisible(false);
+        getContentPane().remove(schuelerErfassenPanel.$$$getRootComponent$$$());
     }
 
     // Quit the application.
     private void quit() {
         System.exit(0);
-    }
-
-    @Override
-    public void internalFrameOpened(InternalFrameEvent ignore) {
-        // empty
-    }
-
-    @Override
-    public void internalFrameClosing(InternalFrameEvent ignore) {
-        // empty
-    }
-
-    @Override
-    public void internalFrameClosed(InternalFrameEvent e) {
-        System.out.println("Frame closed: " + e.getInternalFrame().getTitle());
-    }
-
-    @Override
-    public void internalFrameIconified(InternalFrameEvent ignore) {
-        // empty
-    }
-
-    @Override
-    public void internalFrameDeiconified(InternalFrameEvent ignore) {
-        // empty
-    }
-
-    @Override
-    public void internalFrameActivated(InternalFrameEvent ignore) {
-        // empty
-    }
-
-    @Override
-    public void internalFrameDeactivated(InternalFrameEvent ignore) {
-        // empty
     }
 
 }
