b82cb58ecd94b39e30165ad7c72fa24c79344caf
Hans Stamm <hstamm@hsr.ch>
2015-06-23 22:32:40 +0200

    Deployment mit Installationsanleitung: Erster Wurf

---
+++ bin/createSvmDatabase.bat
@@ -0,0 +1,7 @@
+@ECHO ON
+
+@REM Anlegen der Datenbank svm, des Users svm und der Tabellen in der Datenbank svm.
+
+mysql -u root -p < sql\createDbAndUser.sql
+
+mysql -u svm -psvm < sql\createTables.sql
