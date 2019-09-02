b289bdc6b80c63ca084149b4785899e4ce6d2635
Martin Schraner <marschraner@gmail.com>
2019-09-02 15:23:16 +0200

    E-Mail-Validator korrigiert
    Fehler bei der Eingabe einer E-Mail vom Typ hans.muster@alumni.uni-heidelberg.de (Fehlermeldung Esther, 28.8.2019)

--- src/main/java/ch/metzenthin/svm/common/utils/EmailValidator.java
+++ src/main/java/ch/metzenthin/svm/common/utils/EmailValidator.java
@@ -8,28 +8,23 @@ import java.util.regex.Pattern;
  */
 public class EmailValidator {
 
-    // Source: http://www.mkyong.com/regular-expressions/how-to-validate-email-address-with-regular-expression/
+// Source: http://www.mkyong.com/regular-expressions/how-to-validate-email-address-with-regular-expression/
+// (modified)
 
-    private Pattern pattern;
-
-    private static final String EMAIL_PATTERN =
-            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-    public EmailValidator() {
-        pattern = Pattern.compile(EMAIL_PATTERN);
-    }
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
 
     /**
-     * Validate hex with regular expression
+     * Validate email with regular expression
      *
      * @param email
-     *            hex for validation
-     * @return true valid hex, false invalid hex
+     *            email for validation
+     * @return true valid email, false invalid email
      */
     public boolean isValid(final String email) {
 
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
         return matcher.matches();
 
     }
--- src/test/java/ch/metzenthin/svm/common/utils/EmailValidatorTest.java
+++ src/test/java/ch/metzenthin/svm/common/utils/EmailValidatorTest.java
@@ -16,29 +16,33 @@ public class EmailValidatorTest {
 
     @Test
     public void testValidate_validEmails() {
-        String[]validEmails = new String[] { "mkyong@yahoo.com",
-                "mkyong-100@yahoo.com", "mkyong.100@yahoo.com",
-                "mkyong111@mkyong.com", "mkyong-100@mkyong.net",
-                "mkyong.100@mkyong.com.au", "mkyong@1.com",
-                "mkyong@gmail.com.com", "mkyong+100@gmail.com",
-                "mkyong-100@yahoo-test.com" };
-        for (String email : validEmails) {
-            boolean valid = emailValidator.isValid(email);
-            assertTrue(valid);
-        }
+        assertTrue(emailValidator.isValid("mkyong@yahoo.com"));
+        assertTrue(emailValidator.isValid("mkyong-100@yahoo.com"));
+        assertTrue(emailValidator.isValid("mkyong.100@yahoo.com"));
+        assertTrue(emailValidator.isValid("mkyong111@mkyong.com"));
+        assertTrue(emailValidator.isValid("mkyong-100@mkyong.net"));
+        assertTrue(emailValidator.isValid("mkyong.100@mkyong.com.au"));
+        assertTrue(emailValidator.isValid("mkyong@1.com"));
+        assertTrue(emailValidator.isValid("mkyong@gmail.com.com"));
+        assertTrue(emailValidator.isValid("mkyong+100@gmail.com"));
+        assertTrue(emailValidator.isValid("mkyong-100@yahoo-test.com"));
+        assertTrue(emailValidator.isValid("mk.young@alumni.uni-honkong.de"));
     }
 
     @Test
     public void testValidate_invalidEmails() {
-        String[] invalidEmails = new String[] { "mkyong", "mkyong@.com.my",
-                "mkyong123@gmail.a", "mkyong123@.com", "mkyong123@.com.com",
-                ".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
-                "mkyong..2002@gmail.com", "mkyong.@gmail.com",
-                "mkyong@mkyong@gmail.com", "mkyong@gmail.com.1a" };
-        for (String email : invalidEmails) {
-            boolean valid = emailValidator.isValid(email);
-            assertFalse(valid);
-        }
+        assertFalse(emailValidator.isValid("mkyong"));
+        assertFalse(emailValidator.isValid("mkyong@.com.my"));
+        assertFalse(emailValidator.isValid("mkyong123@gmail.a"));
+        assertFalse(emailValidator.isValid("mkyong123@.com"));
+        assertFalse(emailValidator.isValid("mkyong123@.com.com"));
+        assertFalse(emailValidator.isValid(".mkyong@mkyong.com"));
+        assertFalse(emailValidator.isValid("mkyong()*@gmail.com"));
+        assertFalse(emailValidator.isValid("mkyong@%*.com"));
+        assertFalse(emailValidator.isValid("mkyong..2002@gmail.com"));
+        assertFalse(emailValidator.isValid("mkyong.@gmail.com"));
+        assertFalse(emailValidator.isValid("mkyong@mkyong@gmail.com"));
+        assertFalse(emailValidator.isValid("mkyong@gmail.com.1a"));
     }
 
 }
